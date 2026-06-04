b508e920b2495b4d95dfa54305ac1aa497da0cf2
Aaron <130846565+Astroomind@users.noreply.github.com>
2026-06-04 15:39:48 +0100

    Updated

---
+++ Account.cs
@@ -0,0 +1,7 @@
+public class Account
+{
+    public int Id {get; set;}
+    public string Name {get; set;} = string.Empty;
+    public decimal Balance {get; set;}
+    public byte[] RowVersion {get; set;} = [];
+}
---
+++ Transaction.cs
@@ -0,0 +1,10 @@
+public class Transaction
+{
+    public int Id {get; set;}
+    public int AccountId {get; set;}
+    public decimal Amount {get; set;}
+    public TransactionType TransactionType {get; set;}
+    public DateTime Timestamp {get; set;}
+    public string? Description {get; set;}
+    public Account Account { get; set; } = null!;
+}
